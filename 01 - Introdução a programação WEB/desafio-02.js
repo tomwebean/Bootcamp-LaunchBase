90d684704470f7fdb7c3614b19384e7c373c2b44
Wellington Bernardes <webean@outlook.com>
2020-02-06 13:29:55 -0300

    desafio

--- 01 - Introdução a programação WEB/desafio-02.js
+++ 01 - Introdução a programação WEB/desafio-02.js
@@ -1,3 +1,4 @@
 // https://github.com/Rocketseat/bootcamp-launchbase-desafios-01/blob/master/01-2-lidando-com-objetos-e-vetores.md
 
 // criar um objeto que armazene os dados da Rocketseat
+// imprima em tela a mensagem: A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260

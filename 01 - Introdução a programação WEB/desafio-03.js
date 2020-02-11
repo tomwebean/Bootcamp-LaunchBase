294bf35c00cb7000da1fc0a5a2a69cf4ad535f42
Wellington Bernardes <webean@outlook.com>
2020-02-11 14:24:22 -0300

    tec

--- 01 - Introdução a programação WEB/desafio-03.js
+++ 01 - Introdução a programação WEB/desafio-03.js
@@ -1,3 +1,4 @@
 // https://github.com/Rocketseat/bootcamp-launchbase-desafios-01/blob/master/01-3-funcoes-e-estruturas-de-repeticao.md
 
 // criar um array que armazena nome e suas tecnologias
+// percorrer a lista de usuários com estrutura de repetição

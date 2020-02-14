b6472e1c82522104d17956085eca6d0483c13e02
Wellington Bernardes <webean@outlook.com>
2020-02-14 16:42:47 -0300

    if

--- 01 - Introdução a programação WEB/desafio-03.js
+++ 01 - Introdução a programação WEB/desafio-03.js
@@ -35,3 +35,8 @@ function checaSeUsuarioUsaCSS(usuario) {
 
 for (let i = 0; i < usuarios.length; i++) {
   const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i])
+
+  if (usuarioTrabalhaComCSS) {
+    console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
+  }
+}

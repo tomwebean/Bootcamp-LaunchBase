a98e85139a074dee56f382f87d92012626dbc6b1
Wellington Bernardes <webean@outlook.com>
2020-02-28 11:53:27 -0300

    desafio continuação

--- 01 - Introdução a programação WEB/operadores.js
+++ 01 - Introdução a programação WEB/operadores.js
@@ -14,3 +14,6 @@
 // DESAFIO 1
 const idade = 18
 // verificar se a pessoa é maior igual a 18 anos
+// se sim, deixar entra, se não, bloquear a entrada
+// se a pessoa tiver 17 anos
+// avisar para voltar quando fizer 18 anos

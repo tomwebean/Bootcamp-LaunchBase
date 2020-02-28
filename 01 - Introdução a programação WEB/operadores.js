31737f226ac82851bef3f3e7b5c27cea080cbde0
Wellington Bernardes <webean@outlook.com>
2020-02-28 11:52:50 -0300

    2

--- 01 - Introdução a programação WEB/operadores.js
+++ 01 - Introdução a programação WEB/operadores.js
@@ -4,3 +4,7 @@
 // - (<) Menor
 // - (>=) Maior igual a
 // - (<=) Menor igual a
+// - (==) Igual a
+// - (===) Igual e do mesmo tipo
+// - (!=) Diferente de
+// - (!==) Diferente, inclusive do tipo
